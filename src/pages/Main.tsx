cadacf28346a1605454d42b377eb979b0215d951
Manik Hosen <rahathossenmanik@gmail.com>
2023-12-22 00:50:27 +0600

    Features Pros Section Design

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import TopNavbar from '../components/TopNavbar';
 import HeroSection from '../components/HeroSection';
+import FeaturePros from '../components/FeaturePros';
 
 const Main = () => {
   return (
     <>
       <TopNavbar />
       <HeroSection />
+      <FeaturePros />
     </>
   );
 };

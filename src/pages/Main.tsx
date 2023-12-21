89defa8af4b078c4d6f2e8875e7365d61e374acd
Manik Hosen <rahathossenmanik@gmail.com>
2023-12-22 02:17:07 +0600

    Signup Section Done

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -5,6 +5,7 @@ import FeaturePros from '../components/FeaturePros';
 import Footer from '../components/Footer';
 import TopNFTSection from '../components/TopNFTSection';
 import Collection from '../components/Collection';
+import SignupSection from '../components/SignupSection';
 
 const Main = () => {
   return (
@@ -14,6 +15,7 @@ const Main = () => {
       <FeaturePros />
       <TopNFTSection />
       <Collection />
+      <SignupSection />
       <Footer />
     </>
   );
